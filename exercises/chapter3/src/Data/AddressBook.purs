4899a24845048601d60b8941f3ab133cef2cbcc3
rawtoast <jim@jamesmorris.org>
2018-03-11 17:02:03 +0000

    Chapter 3 exercises

--- exercises/chapter3/src/Data/AddressBook.purs
+++ exercises/chapter3/src/Data/AddressBook.purs
@@ -3,7 +3,7 @@ module Data.AddressBook where
 import Prelude
 
 import Control.Plus (empty)
-import Data.List (List(..), filter, head)
+import Data.List (List(..), filter, head, null, nubBy)
 import Data.Maybe (Maybe)
 
 type Address =
@@ -34,6 +34,33 @@ insertEntry = Cons
 
 findEntry :: String -> String -> AddressBook -> Maybe Entry
 findEntry firstName lastName = head <<< filter filterEntry
+-- same as:
+-- findEntry firstName lastName = filter filterEntry >>> head
   where
   filterEntry :: Entry -> Boolean
   filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+filterEntries :: String -> String -> AddressBook ->  AddressBook
+filterEntries firstName lastName = filter filterEntry
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.firstName == firstName && entry.lastName == lastName
+
+-- 2
+findEntryByAddress :: String -> String -> String -> AddressBook -> Maybe Entry
+findEntryByAddress street city state = filter filterEntry >>> head
+  where
+  filterEntry :: Entry -> Boolean
+  filterEntry entry = entry.address.street == street && entry.address.city == city && entry.address.state == state
+
+-- 3
+hasName :: String -> String -> AddressBook -> Boolean
+hasName firstname lastname = filter filterEntry >>> null >>> eq false
+  where
+  filterEntry entry = entry.firstName == firstname && entry.lastName == lastname
+
+-- 4
+removeDuplicates :: AddressBook -> AddressBook
+removeDuplicates = nubBy filterEntries
+  where
+  filterEntries e1 e2 = e1.firstName == e2.firstName && e1.lastName == e2.lastName
