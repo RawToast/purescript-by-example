d22c9dd3c01f4d275d816522ce66d3d221ba5867
rawtoast <jim@jamesmorris.org>
2018-03-30 11:20:50 +0100

    Chapter 4b solutions

--- exercises/chapter4/src/Exercises.purs
+++ exercises/chapter4/src/Exercises.purs
@@ -1,9 +1,14 @@
 module Exercises where
 
-import Data.Array (null)
-import Data.Array.Partial (head, tail)
-import Partial.Unsafe (unsafePartial)
 import Prelude
+
+import Control.MonadZero (guard)
+import Data.Array (filter, null, (..), concat, (:))
+import Data.Array.Partial (head, tail)
+import Data.FoldableWithIndex (allWithIndex)
+import Data.Functor (voidRight)
+import Partial.Unsafe (unsafePartial)
+import Control.MonadPlus
 
 isEven :: Int -> Boolean
 isEven 0 = true
@@ -24,3 +29,39 @@ countEven arr =
             then (1 + countEven(unsafePartial tail arr))
             else (0 + countEven(unsafePartial tail arr))
 
+-- part b
+squares :: Array Int -> Array Int
+squares numbers = map (\n -> n * n) numbers
+
+noNegative :: Array Int -> Array Int
+noNegative numbers = filter (\n -> n >= 0) numbers
+
+-- (Medium) Define an infix synonym <$?> for filter. Rewrite your answer to the previous question to use your new operator. Experiment with the precedence level and associativity of your operator in PSCi.
+infix 8 filter as <$?>
+
+noNegativeInf :: Array Int -> Array Int
+noNegativeInf ns = (\n -> true) <$?> ns
+
+factors :: Int -> Array (Array Int)
+factors n = do
+  i <- (1 .. n)
+  j <- i .. n
+  guard $ i * j == n
+  pure [i, j]
+
+isPrime :: Int -> Boolean
+isPrime n = length (factors n) == 1
+
+catesian :: Array Int -> Array Int -> Array (Array Int)
+catesian a b = do
+    x <- a
+    y <- b
+    pure [x, y]
+
+triples :: Int -> Array (Array Int)
+triples n = do
+  a <- (1..n)
+  b <- (1..n)
+  c <- (1..n)
+  guard $ (a * a) + (b * b) == c * c 
+  pure [a, b, c]
